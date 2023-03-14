f11fc2818a7e1fb26b5ee34b6dbe01938bac6a98
Vansh Nagpal <you@example.com>
2023-03-14 18:24:15 -0400

    bonus question directory

---
+++ program1/question_bonus/main.cc
@@ -0,0 +1,30 @@
+// Copyright 2023 CSCE350
+// Author vnagpal
+// main method for quicksort numbers in input file
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "functions.h"
+using std::cout, std::endl, std::string, std::vector, std::ofstream;
+int main() {
+  string file_name = "input.txt";
+  vector<double> values = ReadInput("input.txt");
+
+  std::chrono::microseconds elapsed_time;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  QuickSort(&values, 0, values.size() - 1);
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  cout << "Elapsed time: " << elapsed_time.count() << " milliseconds" << endl;
+
+  ofstream out_file("output.txt");
+  for (int i = 0; i < static_cast<int>(values.size()); i++)
+    out_file << values[i] << " ";
+  out_file << endl;
+  return 0;
+}
