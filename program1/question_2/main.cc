2bbfd518f0cbff9fdd388fc55d132962ee73cf71
Vansh Nagpal <you@example.com>
2023-03-14 17:48:47 -0400

    added makefile+ofstream

--- program1/question_2/main.cc
+++ program1/question_2/main.cc
@@ -1,18 +1,18 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "functions.h"
-using std::cout, std::endl, std::string, std::vector;
+using std::cout, std::endl, std::string, std::vector, std::ofstream;
 int main() {
   string file_name = "input.txt";
-  vector<double> unsorted_nums = ReadInput("input.txt");
-  for (int i = 0; i < unsorted_nums.size(); i++)
-    cout << unsorted_nums[i] << " ";
-  cout << "unsorted nums.size() - 1 = " << unsorted_nums.size() - 1 << endl;
-  QuickSort(unsorted_nums, 0, unsorted_nums.size() - 1);
-  for (int i = 0; i < unsorted_nums.size(); i++)
-    cout << unsorted_nums[i] << " ";
-  cout << endl;
+  vector<double> values = ReadInput("input.txt");
+  for (int i = 0; i < values.size(); i++) cout << values[i] << " ";
+  cout << "unsorted nums.size() - 1 = " << values.size() - 1 << endl;
+  QuickSort(values, 0, values.size() - 1);
+  ofstream out_file("output.txt");
+  for (int i = 0; i < values.size(); i++) out_file << values[i] << " ";
+  out_file << endl;
   return 0;
 }
