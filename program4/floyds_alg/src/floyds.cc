772ff61828dbded98548621fee1fc1ea0022eae1
Vansh Nagpal <you@example.com>
2023-04-20 05:52:00 -0400

    linting/documentation

--- program4/floyds_alg/src/floyds.cc
+++ program4/floyds_alg/src/floyds.cc
@@ -4,6 +4,7 @@
  */
 #include "floyds_alg/inc/functions.h"
 int main(int argc, char** argv) {
+  // runs floyds algorithm on weight matrix specified in input.txt
   FloydsAlgorithm("input.txt");
   return 0;
 }
