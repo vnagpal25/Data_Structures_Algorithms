a6cf1176947302d3a4e0df79910aabd73c1a0ef4
Vansh Nagpal <you@example.com>
2023-04-20 21:43:52 -0400

    linting/documentation

--- program4/max_bipartite_matching/inc/functions.h
+++ program4/max_bipartite_matching/inc/functions.h
@@ -1,64 +1,183 @@
-#ifndef PROGRAM4_MAX_BIPARTITE_MATCHING_ALG_INC_FUNCTIONS_H_
+/**
+ * Copyright 2023 - Class Declaration for Maximum Bipartite Matching
+ * @author vnagpal
+*/
+#ifndef PROGRAM4_MAX_BIPARTITE_MATCHING_INC_FUNCTIONS_H_
 #define PROGRAM4_MAX_BIPARTITE_MATCHING_INC_FUNCTIONS_H_
-#include <unistd.h>
+#include <unistd.h>  // sleep() for debugging
 
-#include <algorithm>
-#include <chrono>    // for execution times
-#include <cmath>     // floor()
-#include <fstream>   // I/O to files
-#include <iostream>  // I/O
-#include <queue>
-#include <string>  // string functionality
-#include <utility>
-#include <vector>                                       // vector functionality
-using std::string, std::vector, std::queue, std::pair;  // readability
+#include <algorithm>  // std::find()
+#include <cmath>      // floor()
+#include <fstream>    // I/O to files
+#include <iostream>   // I/O
+#include <queue>      // queue functionality
+#include <string>     // string functionality
+#include <utility>    // pair data structure functionality
+#include <vector>     // vector functionality
+using std::string, std::vector, std::queue, std::pair,
+    std::ostream;  // readability
+// MaxBipartiteMatching Class for organization/housekeeping
 class MaxBipartiteMatching {
  public:
+  // struct to represent a Node in the graph
   struct Node {
+    // identifier of node (1,2,3..., n)
     int node_num_;
-    bool is_free_;
-    int label = 0;
+
+    // defines node's match status
+    bool free_;
+
+    // label for augmenting paths, points to node_num of other node
+    int label_ = 0;
+
+    /**
+     * Overloading == operator for a Node
+     * @param node node being compared to
+     * @return true if both nodes have the same identifier, false otherwise
+     */
+    bool operator==(const Node& node) const {
+      return (node.node_num_ == node_num_);
+    }
   };
 
-  void PrintResults();
+  // redefining pair of two Nodes as NodePair
+  typedef pair<Node, Node> NodePair;
 
-  void ModifyInMatching(Node v, int label);
-
-  void ModifyInQueue(Node v, int label);
-
-  void SetNodeLabel(Node v, int label);
-
-  Node LabelMate(Node w);
-
-  void UpdateFreedoms();
-
-  void ReinitializeQueue();
-
-  void RemoveLabels();
-
-  void Add(pair<Node, Node> toAdd);
-
-  void Remove(pair<Node, Node> toRemove);
-
-  Node GetNodeByValue(int node_val);
-
-  bool Contains(vector<pair<Node, Node>> pairs, pair<Node, Node> pair);
-
-  bool Contains(vector<Node> nodes, Node node);
-
-  bool IsAdjacent(int first_node_val, int second_node_val, int** adj_mat);
-
-  void ParseLine(vector<Node>* V, string line);
-
-  void ReadInput(string file_name);
-
+  /**
+   * Implements the Maximum Bipartite Matching Algorithm defined in textbook
+   * @param file_name name of file which contains the sets V, U, and the
+   * adjacency matrix of the bipartite graph G
+   */
   void MaxBipartiteMatch(string file_name);
 
+  ~MaxBipartiteMatching();
+
  private:
-  vector<Node> V, U;
-  int size_;
-  int** adj_mat;
-  vector<pair<Node, Node>> M;  // M contains all of the matched pairs
-  queue<Node> Q;
+  /**
+   * Overloading == operator for a pair of Nodes
+   * @param left_pair lhs pair
+   * @param right_pair rhs pair
+   * @return true if both pairs have equivalent nodes, false otherwise
+   */
+  friend bool operator==(const NodePair& left_pair,
+                         const NodePair& right_pair) {
+    return (left_pair.first == right_pair.first &&
+            left_pair.second == right_pair.second) ||
+           (left_pair.first == right_pair.second &&
+            left_pair.second == right_pair.first);
+  }
+
+  /**
+   * Prints the results of the Maximum Bipartite Matching Algorithm to output
+   * file
+   */
+  void PrintResults();
+
+  /**
+   * Modifies the label of a particular node in the matching set
+   * @param v node who's label needs to be modified
+   * @param label number of the node with which v needs to be labeled with, used
+   * for augmenting paths
+   */
+  void ModifyInMatching(Node v, int label);
+
+  /**
+   * Modifies the label of a particular node in the queue
+   * @param v node who's label needs to be modified
+   * @param label number of the node with which v needs to be labeled with, used
+   * for augmenting paths
+   */
+  void ModifyInQueue(Node v, int label);
+
+  /**
+   * Modifies the label of a particular node in V, U, Q, and M
+   * @param v node who's label needs to be modified
+   * @param label number of the node with which v needs to be labeled with, used
+   * for augmenting paths
+   */
+  void SetNodeLabel(Node v, int label);
+
+  /**
+   * Given a matched node, label its mate with its identifier and return the
+   * labeled mate
+   * @param w matched node whose mate needs to be labeled with its identifier
+   * @return the labeled mate of w
+   */
+  Node LabelMate(Node w);
+
+  /**
+   * Updates the .free_ member of each node in V and U based on the matching M
+   */
+  void UpdateFreedoms();
+
+  /**
+   * Empties and initializes Q with free vertices in V
+   */
+  void ReinitializeQueue();
+
+  /**
+   * Removes the labels of each node in V and U
+   */
+  void RemoveLabels();
+
+  /**
+   * Adds a pair to the matching
+   * @param to_add pair of Nodes in V and U that need to be added to the
+   * matching M
+   */
+  void Add(NodePair to_add);
+
+  /**
+   * Removes a pair from the matching
+   * @param to_remove pair of Nodes in V and U that need to be removed from the
+   * matching
+   */
+  void Remove(NodePair to_remove);
+
+  /**
+   * returns a node specified by node_val from U or V
+   * @param node_val identifier of desired node
+   * @return Node from V or U
+   */
+  Node GetNodeByValue(int node_val);
+
+  /**
+   * Checks if a set of values contain a particular element
+   * @param element element whose existence in the set is being checked
+   * @param set set of elements
+   * @return true if element ϵ set, false if not
+   */
+  template <class T>
+  bool Contains(vector<T> set, T element);
+
+  /**
+   * Checks if two nodes are adjacent in the Graph specified by the adjacency
+   * matrix
+   * @param first_node identifier of the first node
+   * @param second_node identifier of the second node
+   * @return true if both nodes are adjacent, false if not
+   */
+  bool IsAdjacent(int first_node, int second_node);
+
+  /**
+   * Takes a string of numbers and populates a vector with its contents
+   * @param V pass by pointer variable, will contain nodes based on integers in
+   * line
+   * @param line line that contains integers (1,... n)
+   */
+  void ParseLine(vector<Node>* V, string line);
+
+  /**
+   * Reads an input file and populates V, U, and adj_mat
+   * @param file_name name of input file
+   */
+  void ReadInput(string file_name);
+
+  vector<Node> V, U;   // bipartite vertex sets
+  vector<NodePair> M;  // M contains all of the matched pairs
+  queue<Node> Q;       // bfs queue of nodes being explored
+
+  int size_;      // number of nodes in graph
+  int** adj_mat;  // adjacency matrix of graph
 };
 #endif  // PROGRAM4_MAX_BIPARTITE_MATCHING_INC_FUNCTIONS_H_
