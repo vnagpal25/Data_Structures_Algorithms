0a5634c9fe3c5bb5f9c143d13a7582877e3c285c
Vansh Nagpal <you@example.com>
2023-04-22 08:50:40 -0400

    linting/documentation

--- program4/max_bipartite_matching/src/max_bipartite.cc
+++ program4/max_bipartite_matching/src/max_bipartite.cc
@@ -1,6 +1,12 @@
-#include "max_bipartite_matching/inc/functions.h"
+/**
+ * Copyright 2023 - CSCE350
+ * Main Entry point for the Maximum Bipartite Matching Algorithm
+ * @author vnagpal
+ */
+#include "max_bipartite_matching/inc/functions.h"  // class header
 int main(int argc, char** argv) {
+  // Runs algorithm
   MaxBipartiteMatching max_bipartite_matching;
-  max_bipartite_matching.MaxBipartiteMatch("input2.txt");
+  max_bipartite_matching.MaxBipartiteMatch("input.txt");
   return 0;
 }
